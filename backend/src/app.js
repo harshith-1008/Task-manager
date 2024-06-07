b8512e34950303696ecaf7657b1d6bad3ccda657
harshith-1008 <saiharshith.poli@gmail.com>
2024-06-07 12:10:12 +0530

    added options

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,6 +15,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+app.options("*", cors());
 
 import userRouter from "./routes/user.route.js";
 import boardRouter from "./routes/board.route.js";
