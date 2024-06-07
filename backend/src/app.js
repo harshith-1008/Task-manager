b2ae13e92e6391e645e7028463bffcd9f9a5ad61
harshith-1008 <saiharshith.poli@gmail.com>
2024-06-07 12:26:19 +0530

    changed origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: ["https://task-manager-frontend-gold-zeta.vercel.app"],
     credentials: true,
   })
 );
