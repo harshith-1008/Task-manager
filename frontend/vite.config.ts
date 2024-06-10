262d1c8120b17fde8495b03c8304ef4ea37a2e96
harshith-1008 <saiharshith.poli@gmail.com>
2024-06-10 21:38:52 +0530

    removed proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "https://task-manager-api-v2kf.onrender.com",
-    },
-  },
+  server: {},
 });
